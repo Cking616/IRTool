c5a4e0f40d535bd2ac19b9553f39e632db6b5d92
cking616 <cking616@mail.ustc.edu.cn>
2018-04-10 17:40:49 +0800

    commit change

---
+++ IRTool/IRRobot.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.ProtoBase;
+using SuperSocket.ClientEngine;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace IRTool
+{
+    /// <summary>
+    /// 使用Log4net插件的log日志对象
+    /// </summary>
+    public static class AppLog
+    {
+        private static Queue<string> IrRobotRevLog = new Queue<string>();
+
+        public static void AddLog(string log) { IrRobotRevLog.Enqueue(log); }
+
+        public static void Info(string cls, string log)
+        {
+            string str = cls + ":" + log;
+            IrRobotRevLog.Enqueue(str);
+        }
+
+        public static string ReadLog() { return IrRobotRevLog.Dequeue(); }
+
+        public static bool IsEmpty() { return IrRobotRevLog.Count == 0; }
+
+        public static void Error(string err)
+        {
+            err = "Error:" + err;
+            IrRobotRevLog.Enqueue(err);
+        }
+    }
+
+    class IRRobotFilter : BeginEndMarkReceiveFilter<StringPackageInfo>
+    {
+        //开始和结束标记也可以是两个或两个以上的字节
+        private readonly static byte[] BeginMark = new byte[] { (byte)'!' };
+        private readonly static byte[] EndMark = new byte[] { (byte)'\n' };
+        
+
+        public IRRobotFilter()
+            : base(BeginMark, EndMark) //传入开始标记和结束标记
+        {
+
+        }
+
+        public override StringPackageInfo ResolvePackage(IBufferStream bufferStream)
+        {
+            // other code you need implement according yoru protocol details
+            byte[] bytes = new byte[bufferStream.Length];
+            bufferStream.Read(bytes, 0, bytes.Length);
+            string str = System.Text.Encoding.ASCII.GetString(bytes);
+            AppLog.Info("Ir接收", str);
+            string[] strArry = str.Split(' ');
+            string strHead = strArry[1];
+            string[] strParam = strArry.Skip(2).ToArray();
+            string[] strHeads = strHead.Split('-');
+            if (strHeads.Length < 2)
+            {
+                return new StringPackageInfo(strHead, "", strParam);
+            }
+            else
+            {
+                string strBody = strHeads[0];
+                strHead = strHeads[1];
+                return new StringPackageInfo(strHead, strBody, strParam);
+            }
+        }
+    }
+
+    class IRRobot
+    {
+        private EasyClient irClient;
+        private string irAddress = "";
+        private int irPort = 5000;
+        private bool irNeedReset = false;
+        private bool irIsIdle = true;
+        private string irLastSend = "";
+        private Queue<string> irSendBuffer = new Queue<string>();
+
+        public bool IsConnected { get => irClient.IsConnected; }
+        public bool IrNeedReset { get => irNeedReset;}
+        public bool IrIsIdle { get => irIsIdle; }
+
+
+        public IRRobot(string address, int port)
+        {
+            irAddress = address;
+            irPort = port;
+            irClient = new EasyClient();
+
+            irClient.Connected += OnConnected;
+            irClient.Closed += OnClosed;
+            // Initialize the client with the receive filter and request handler
+            irClient.Initialize(new IRRobotFilter(), OnRecieve);
+            irClient.ConnectAsync(new IPEndPoint(IPAddress.Parse(irAddress), irPort));
+
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(50)
+            };
+            timer.Tick += OnTimer;
+            timer.IsEnabled = true;
+        }
+
+        private void OnConnected(Object state, EventArgs e) => AppLog.Info("系统", "成功与Ir控制器建立连接");
+
+        private void OnClosed(Object state, EventArgs e) => AppLog.Info("系统", "与Ir控制器连接断开");
+
+        public bool SendCmd(string cmd)
+        {
+            irSendBuffer.Enqueue(cmd);
+            return true;
+        }
+
+        private bool __SendCmd(string cmd)
+        {          
+            if (!irIsIdle)
+            {
+                return false;
+            }
+
+            string send = cmd;
+            if (cmd.IndexOf("\n") != cmd.Length - 1)
+                send = cmd + "\n";
+
+            irClient.Send(Encoding.ASCII.GetBytes(send));
+            irLastSend = cmd;
+            irIsIdle = false;
+            return true;
+        }
+
+        private void OnRecieve(StringPackageInfo request)
+        {
+            if (request.Key == "ERROR")
+            {
+                irNeedReset = true;
+                AppLog.Info("系统", "收到错误返回，自动Reset");
+                SendCmd("RESET\n");
+            }
+
+            if (request.Key == "RESET" && request.Body == "END")
+            {
+                AppLog.Info("系统", "Reset成功");
+                irNeedReset = false;
+                irIsIdle = true;
+                return;
+            }
+
+            if(request.Key == irLastSend && request.Body == "END")
+            {
+                irIsIdle = true;
+            }
+        }
+
+        private void OnTimer(Object state, EventArgs e)
+        {
+            if(irSendBuffer.Count > 0)
+            {
+                string cmd = irSendBuffer.ElementAt(0);
+                if(__SendCmd(cmd))
+                {
+                    irSendBuffer.Dequeue();
+                }
+            }
+        }
+    }
+}
--- IRTool/MainWindow.xaml.cs
+++ IRTool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace IRTool
 {
@@ -22,14 +24,25 @@ namespace IRTool
     {
         const string IRCmdsPath = "./IRCmds.txt";
         const string IRStationPath = "./IRStation.txt";
-
+        IRRobot irRobot = null;
         public MainWindow()
         {
             InitializeComponent();
 
-            IrSendText_Initiate();
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(150)
+            };
+            timer.Tick += OnTimer;
+            timer.IsEnabled = true;
 
+            IrSendText_Initiate();
             IrStationTextBox_Initiate();
+        }
+
+        private void ExitApp()
+        {
+
         }
 
         private void IrSendText_Initiate()
@@ -55,6 +68,133 @@ namespace IRTool
                 {
                     IrStationTextBox.AddItem(new AutoCompleteEntry(line, null));
                 }
+            }
+        }
+
+        public void IrConnect_Click(object sender, RoutedEventArgs e)
+        {
+            irRobot = new IRRobot(IrAddress.Text, 5001);
+        }
+
+        public void IrSend_Click(object sender, RoutedEventArgs e)
+        {
+            String S = IrSendTextBox.Text;
+            if (S == "")
+            {
+                return;
+            }
+
+            IrSendTextBox.AddItem(new AutoCompleteEntry(S, null));
+
+            if (irRobot.IsConnected)
+            {
+                irRobot.SendCmd(S);
+            }
+            else
+            {
+                AppLog.Error("IrRobot didn't connected");
+            }
+
+            IrSendTextBox.Text = "";
+        }
+
+        public void IrFilePath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Filter = "文本文件|*.txt"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                IrScriptFilePath.Text = dialog.FileName + dialog.SafeFileName;
+            }
+        }
+
+        public void IrSendFile_Click(object sender, RoutedEventArgs e)
+        {
+            string FilePath = IrScriptFilePath.Text;
+            if (File.Exists(FilePath))
+            {
+                StreamReader sr = new StreamReader(FilePath, Encoding.Default);
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                }
+            }
+            else
+            {
+                MessageBox.Show("文件不存在，请选择正确的文件然后重试");
+            }
+        }
+
+        public void IrExit_Click(object sender, RoutedEventArgs e)
+        {
+            ExitApp();
+            Environment.Exit(0);
+        }
+
+        private void IrRobot_Process()
+        {
+            if (IrSendCmd.IsEnabled)
+            {
+                if (!irRobot.IsConnected)
+                {
+                    IrSendCmd.IsEnabled = false;
+                    IrConnect.IsEnabled = true;
+                    IrSendFile.IsEnabled = false;
+                }
+
+                if (irRobot.IrNeedReset)
+                {
+                    IrSendCmd.IsEnabled = false;
+                    IrConnect.IsEnabled = true;
+                    IrSendFile.IsEnabled = false;
+                }
+            }
+            else
+            {
+                if (irRobot != null)
+                {
+                    if (irRobot.IsConnected && !irRobot.IrNeedReset)
+                    {
+                        IrSendCmd.IsEnabled = true;
+                        IrConnect.IsEnabled = false;
+                        IrSendFile.IsEnabled = true;
+                    }
+                }
+                else
+                {
+                    IrSendCmd.IsEnabled = false;
+                    IrSendFile.IsEnabled = false;
+                    IrConnect.IsEnabled = true;
+                }
+            }
+        }
+
+        public void IrRecvMsgs(String S) => Dispatcher.BeginInvoke(new Action(() =>
+                                          {
+                                              if (IrMsgsBox.Text.Length == 0)
+                                                  IrMsgsBox.Text = S;
+                                              else
+                                              {
+                                                  if (S.IndexOf("\n") != S.Length - 1)
+                                                      IrMsgsBox.Text += S + "\n";
+                                                  else
+                                                      IrMsgsBox.Text += S;
+                                              }
+                                              IrMsgsBox.Focus();
+                                              IrMsgsBox.CaretIndex = IrMsgsBox.Text.Length;
+                                          }));
+
+
+        public void OnTimer(Object state, EventArgs e)
+        {
+            IrRobot_Process();
+
+            while (!AppLog.IsEmpty())
+            {
+                string str = AppLog.ReadLog();
+                IrRecvMsgs(str);
             }
         }
     }
