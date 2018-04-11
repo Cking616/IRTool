2605e309019108334977cd523566225a3cb37d41
cking616 <cking616@mail.ustc.edu.cn>
2018-04-11 18:19:55 +0800

    new commit

--- IRTool/IRRobot.cs
+++ IRTool/IRRobot.cs
@@ -34,188 +34,59 @@ namespace IRTool
         }
     }
 
-    class IRRobotFilter : IReceiveFilter<StringPackageInfo>
+    class IRRobotFilter : TerminatorReceiveFilter<StringPackageInfo>
     {
-        private readonly SuperSocket.ProtoBase.SearchMarkState<byte> m_BeginSearchState;
-        private readonly SuperSocket.ProtoBase.SearchMarkState<byte> m_EndSearchState;
-        private readonly SuperSocket.ProtoBase.SearchMarkState<byte> m_EndSearchState2;
-        private bool m_FoundBegin = false;
-
         public IRRobotFilter()
+            : base(Encoding.ASCII.GetBytes("\r\n"))
         {
-            m_BeginSearchState = new SuperSocket.ProtoBase.SearchMarkState<byte>(new byte[] {(byte)'>'});
-            m_EndSearchState = new SuperSocket.ProtoBase.SearchMarkState<byte>(Encoding.ASCII.GetBytes("!E"));
-            m_EndSearchState2 = new SuperSocket.ProtoBase.SearchMarkState<byte>(new byte[] { (byte)'\r' });
         }
 
-        private bool CheckChanged(byte[] oldMark, byte[] newMark)
-        {
-            if (oldMark.Length != newMark.Length)
-                return true;
-
-            for (var i = 0; i < oldMark.Length; i++)
-            {
-                if (oldMark[i] != newMark[i])
-                    return true;
-            }
-
-            return false;
-        }
-
-        public void ChangeBeginMark(byte[] beginMark)
-        {
-            if (!CheckChanged(m_BeginSearchState.Mark, beginMark))
-                return;
-
-            m_BeginSearchState.Change(beginMark);
-        }
-
-        public void ChangeEndMark(byte[] endMark)
-        {
-            if (!CheckChanged(m_EndSearchState.Mark, endMark))
-                return;
-
-            m_EndSearchState.Change(endMark);
-        }
-
-        public StringPackageInfo ResolvePackage(IBufferStream bufferStream)
+        public override StringPackageInfo ResolvePackage(IBufferStream bufferStream)
         {
             byte[] bytes = new byte[bufferStream.Length];
             bufferStream.Read(bytes, 0, bytes.Length);
             string str = System.Text.Encoding.ASCII.GetString(bytes);
             AppLog.AddLog("Ir接收:\n" + str);
 
-            string[] str1 = str.Split('\r');
-            string[] strArry = str1.ElementAt(str1.Length - 2).Split(' ');
-            string strHead = strArry[1];
-            string[] strParam = strArry.Skip(2).ToArray();
-            string[] strHeads = strHead.Split('-');
-            if (strHeads.Length < 2)
+            if(str.StartsWith(">"))
             {
-                return new StringPackageInfo(strHead, "", strParam);
+                return new StringPackageInfo("BEGIN", "", null);
+            }
+
+            if (str.StartsWith("!"))
+            {
+                string[] strArry = str.Split(' ');
+                string strHead = strArry[1];
+                string[] strParam = strArry.Skip(2).ToArray();
+                string[] strHeads = strHead.Split('-');
+                if (strHeads.Length < 2)
+                {
+                    return new StringPackageInfo(strHead, "", strParam);
+                }
+                else
+                {
+                    string strBody = strHeads[0];
+                    strHead = strHeads[1];
+                    return new StringPackageInfo(strHead, strBody, strParam);
+                }
             }
             else
             {
-                string strBody = strHeads[0];
-                strHead = strHeads[1];
-                return new StringPackageInfo(strHead, strBody, strParam);
+                string[] strArry = str.Split(' ');
+                string strHead = strArry[0];
+                string[] strParam = strArry.Skip(2).ToArray();
+                string[] strHeads = strHead.Split('-');
+                if (strHeads.Length < 2)
+                {
+                    return new StringPackageInfo(strHead, "", strParam);
+                }
+                else
+                {
+                    string strBody = strHeads[0];
+                    strHead = strHeads[1];
+                    return new StringPackageInfo(strHead, strBody, strParam);
+                }
             }
-        }
-
-        public virtual StringPackageInfo Filter(BufferList data, out int rest)
-        {
-            rest = 0;
-
-            int searchEndMarkOffset;
-            int searchEndMarkLength;
-
-            var currentSegment = data.Last;
-            var readBuffer = currentSegment.Array;
-            var offset = currentSegment.Offset;
-            var length = currentSegment.Count;
-
-            int totalParsed = 0;
-
-            if (!m_FoundBegin)
-            {
-                int pos = readBuffer.SearchMark(offset, length, m_BeginSearchState, out totalParsed);
-
-                if (pos < 0)
-                {
-                    //All received data is part of the begin mark
-                    if (m_BeginSearchState.Matched > 0 && data.Total == m_BeginSearchState.Matched)
-                        return new StringPackageInfo("Err-Package", "", null);
-
-                    //Invalid data, contains invalid data before the regular begin mark
-                    State = FilterState.Error;
-                    return new StringPackageInfo("Err-Package", "", null);
-                }
-
-                //Found the matched begin mark
-                if (pos != offset)//But not at the beginning, contains invalid data before the regular begin mark
-                {
-                    State = FilterState.Error;
-                    return new StringPackageInfo("Err-Package", "", null);
-                }
-
-                //Found start mark, then search end mark
-                m_FoundBegin = true;
-
-                searchEndMarkOffset = offset + totalParsed;
-
-                //Reach end
-                if (offset + length <= searchEndMarkOffset)
-                    return new StringPackageInfo("Err-Package", "", null);
-
-                searchEndMarkLength = offset + length - searchEndMarkOffset;
-            }
-            else//Already found begin mark
-            {
-                searchEndMarkOffset = offset;
-                searchEndMarkLength = length;
-            }
-
-            while (true)
-            {
-                var endPos = readBuffer.SearchMark(searchEndMarkOffset, searchEndMarkLength, m_EndSearchState, out int parsedLength);
-
-                //Haven't found end mark
-                if (endPos < 0)
-                {
-                    return new StringPackageInfo("Err-Package", "", null);
-                }
-
-                totalParsed += parsedLength; //include begin mark if the mark is found in this round receiving
-                rest = length - totalParsed;
-
-                searchEndMarkOffset = offset + totalParsed;
-                searchEndMarkLength = offset + length - searchEndMarkOffset;
-                //if (rest > 0)
-                //    data.SetLastItemLength(totalParsed);
-
-                endPos = readBuffer.SearchMark(searchEndMarkOffset, searchEndMarkLength, m_EndSearchState2, out parsedLength);
-
-                //Haven't found end mark
-                if (endPos < 0)
-                {
-                    return new StringPackageInfo("Err-Package", "", null);
-                }
-
-                totalParsed += parsedLength; //include begin mark if the mark is found in this round receiving
-                rest = length - totalParsed;
-
-                if (rest > 0)
-                    data.SetLastItemLength(totalParsed);
-
-                var packageInfo = ResolvePackage(this.GetBufferStream(data));
-
-                if (!ReferenceEquals(packageInfo, default(StringPackageInfo)))
-                {
-                    Reset();
-                    return packageInfo;
-                }
-
-                if (rest > 0)
-                {
-                    searchEndMarkOffset = endPos + m_EndSearchState.Mark.Length;
-                    searchEndMarkLength = rest;
-                    continue;
-                }
-
-                //Not found end mark
-                return new StringPackageInfo("Err-Package", "", null);
-            }
-        }
-
-        public IReceiveFilter<StringPackageInfo> NextReceiveFilter { get; protected set; }
-
-        public FilterState State { get; protected set; }
-
-        public void Reset()
-        {
-            m_BeginSearchState.Matched = 0;
-            m_EndSearchState.Matched = 0;
-            m_FoundBegin = false;
         }
     }
 
@@ -285,7 +156,7 @@ namespace IRTool
 
         private void OnRecieve(StringPackageInfo request)
         {
-            if (request.Key == "Err-Package")
+            if (request.Key == "BEGIN")
             {
                 return;
             }
